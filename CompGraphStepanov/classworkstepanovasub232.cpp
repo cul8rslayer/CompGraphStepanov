e79539e9934d3994181ea6f9f5fb7f2bdb57beda
Alexey <150338165+cul8rslayer@users.noreply.github.com>
2025-02-23 21:36:52 +0800

    Update classworkstepanovasub232.cpp

--- CompGraphStepanov/classworkstepanovasub232.cpp
+++ CompGraphStepanov/classworkstepanovasub232.cpp
@@ -1,20 +1,18 @@
+#include <cstdio>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 #define GLEW_DLL
 #define GLFW_DLL
 
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-#include "stdio.h"
-#include "stdlib.h"
 
 int main()
 {
-    printf("Hello World!\n");
-
     glfwInit();
+    // printf("Hello World!\n");
 
     if (!glfwInit()) {
-        fprintf(stderr, "ERROR glfwInit: \n");
-        return -1;
+        fprintf(stderr, "ERROR: could not start GLFW. \n");
+        return 1;
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
@@ -22,22 +20,20 @@ int main()
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = 0;
-    glfwCreateWindow(512, 512, "Дикая программа", NULL, NULL);
+    GLFWwindow* window;
+    window = glfwCreateWindow(512, 512, "window", NULL, NULL);
 
     if (!window) {
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
     glfwMakeContextCurrent(window);
-
     glewExperimental = GL_TRUE;
 
     GLenum ret = glewInit();
-
     if (GLEW_OK != ret) {
-        fprintf(stderr, "ERROR $s\n", glewGetErrorString(ret));
+        fprintf(stderr, "ERROR: %s\n", glewGetErrorString(ret));
         return 1;
     }
 
@@ -45,13 +41,17 @@ int main()
         glClearColor(0.5, 0.5, 0.5, 1);
         glClear(GL_COLOR_BUFFER_BIT);
         glBegin(GL_TRIANGLES);
-        glVertex2f(0.0, 0.5); glColor3f(0.0, 1.0, 0.0);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, -0.5);
-
+        glColor3f(0.9, 0.0, 0.0);
+        glVertex2f(0.0, 1.0);
+        glColor3f(1.0, 0.5, 0.0);
+        glVertex2f(1, -0.5);
+        glColor3f(1.0, 0.5, 1.0);
+        glVertex2f(1, 0);
+        glEnd();
         glfwSwapBuffers(window);
         glfwPollEvents();
-    };
-
+    }
     glfwTerminate();
+
+
 }
